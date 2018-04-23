ca2ed0b8c94844d158a743d26bbcec5412acbe8a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfSpreadsheetMailMerge/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using DevExpress.Spreadsheet;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Spreadsheet;
+using System.Collections.Generic;
+using System;
+
+namespace WpfSpreadsheetMailMerge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Core.ThemedWindow
+    {
+        ThemedWindow previewWindow;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateTemplate();
+        }
+
+        IWorkbook Workbook { get { return spreadsheetControl.Document; } }
+        DefinedName MailMergeMode { get { return Workbook.DefinedNames.GetDefinedName("MAILMERGEMODE"); } }
+        
+        public void CreateTemplate()
+        {
+            // Create a mail merge template.
+            Worksheet template = Workbook.Worksheets[0];
+            template.Cells["C2"].Formula = "FIELDPICTURE(\"Photo\", \"range\", C2, FALSE, 50)";
+            template.Cells["C3"].Formula = "=FIELD(\"FirstName\")&\" \"&FIELD(\"LastName\")";
+            template.Cells["B4"].Value = "Position:";
+            template.Cells["C4"].Formula = "FIELD(\"Title\")";
+            template.Cells["B5"].Value = "Birth Date:";
+            template.Cells["C5"].Formula = "FIELD(\"BirthDate\")";
+            template.Cells["C5"].NumberFormat = "MMMM dd, yyyy";
+            template.Cells["B6"].Value = "Hire Date:";
+            template.Cells["C6"].Formula = "FIELD(\"HireDate\")";
+            template.Cells["C6"].NumberFormat = "MMMM dd, yyyy";
+            template.Cells["B7"].Value = "Home Phone:";
+            template.Cells["C7"].Formula = "FIELD(\"HomePhone\")";
+            template.Cells["B8"].Value = "Address:";
+            template.Cells["C8"].Formula = "=FIELD(\"Address\")&\" \"&FIELD(\"City\")";
+            template.Cells["B9"].Value = "About:";
+            template.Cells["C9"].Formula = "FIELD(\"Notes\")";
+
+            // Set a detail range in the template.
+            Range detail = template.Range["C1:C9"];
+            detail.Name = "DETAILRANGE";
+
+            // Set a header range in the template.
+            Range header = template.Range["B1:B9"];
+            header.Name = "HEADERRANGE";
+
+            SetDefaultMailMergeOptions();
+        }
+
+        public void SetDefaultMailMergeOptions()
+        {
+            // Set the mail merge mode to "Multiple Sheets".
+            Workbook.DefinedNames.Add("MAILMERGEMODE", "\"Worksheets\"");
+
+            // Set the document orientation.
+            Workbook.DefinedNames.Add("HORIZONTALMODE", "TRUE");
+        }
+
+        public void ShowPreview()
+        {
+            if (previewWindow != null)
+                previewWindow.Close();
+
+            PerformMailMerge();
+            previewWindow = new ThemedWindow();
+            previewWindow.Owner = this;
+            previewWindow.Title = "Mail Merge Preview";
+            SpreadsheetControl control = new SpreadsheetControl();
+            control.Document.LoadDocument("MailMergeResult.xlsx");
+            previewWindow.Content = control;
+            previewWindow.Show();
+        }
+
+        public void PerformMailMerge()
+        {
+            // Perform a mail merge.
+            Workbook.MailMergeDataSource = EmployeesInfo.GetData();
+            IList<IWorkbook> result = Workbook.GenerateMailMergeDocuments();
+            result[0].SaveDocument("MailMergeResult.xlsx");
+        }
+
+        public bool CanSelectMode()
+        {
+            return MailMergeMode != null;
+        }
+
+        public void SelectSingleSheetMode()
+        {
+            MailMergeMode.RefersTo = "\"OneWorksheet\"";
+        }
+
+        public void SelectMultipleSheetsMode()
+        {
+            MailMergeMode.RefersTo = "\"Worksheets\"";
+        }
+
+        public void SelectMultipleDocumentsMode()
+        {
+            MailMergeMode.RefersTo = "\"Documents\"";
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# WPF Spreadsheet - How to perform a mail merge
+
+
+This example demonstrates how to use the <strong>Spreadsheet Mail Merge</strong> functionality to automatically generate a document based on data retrieved from a data source. <br>The mail merge template is created in code when the application starts. The <a href="https://documentation.devexpress.com/CoreLibraries/DevExpress.Spreadsheet.IWorkbook.MailMergeDataSource.property">IWorkbook.MailMergeDataSource</a> property specifies the data source. The <a href="https://documentation.devexpress.com/CoreLibraries/DevExpress.Spreadsheet.IWorkbook.GenerateMailMergeDocuments.method">IWorkbook.GenerateMailMergeDocuments</a> method accomplishes mail merge and returns the resulting workbook.<br>The <strong>Mail Merge</strong> tab is added to the SpreadsheetControl's ribbon to provide end-users with capabilities to select the mail merge mode and preview the merged document. If the <strong>Multiple Documents</strong> mode is used, only the first merged workbook is shown in the preview window.<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/wpf-spreadsheet-how-to-perform-a-mail-merge-t618180/17.1.3+/media/c9160390-a446-4e31-b83a-b290731e5fa6.png">
+
+<br/>
+
+
